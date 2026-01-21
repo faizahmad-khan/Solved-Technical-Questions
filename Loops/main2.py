d9536fd4d76f61cd2152214aa17265ea49377256
faizahmad-khan <amanfaiz92@gmail.com>
2026-01-21 11:37:06 +0530

    first commit

---
+++ Loops/main2.py
@@ -0,0 +1,15 @@
+# # #print table
+# # n = int (input ("enter the number"))
+# # for i in range (1,11):
+# #     print(i*5)
+# for i in range (5,1,-1):
+#    for j in range(1,i+1):
+#      print(j,end =" ")
+#    print()
+#factorial of a number
+
+n= 5
+fact = 1
+for i in range (1,n+1):
+ fact = fact*i 
+print(fact)
